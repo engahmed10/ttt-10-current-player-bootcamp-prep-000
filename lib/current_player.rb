76e0892ce974e4741af9871cd94376a62d0719cb
AHMED AFANDY <eng.computer1@yahoo.com>
2020-02-27 05:21:43 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -10,8 +10,12 @@ end
 
 
 def current_player(board)
-
-      turn_count(board) %2 == 0  ? "X":"O"
+      if turn_count(board) %2 == 0
+         "X"
+       else
+         "O"
+      end
+    #  turn_count(board) %2 == 0  ? "X":"O"
 
   end
 end
